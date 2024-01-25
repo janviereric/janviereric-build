8d68ff9dc551e2b6d5e5a12dd2b5f4274d627914
janviereric <janviereric@icloud.com>
2024-01-25 18:09:14 +0100

    Mise en place du header

--- dist/assets/index-yFwrEQja.js
+++ dist/assets/index-yFwrEQja.js
@@ -1,4 +1,4 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const r="/assets/logo-6Mk-ll80.jpg",c=()=>{const t=document.querySelector("header");t.innerHTML=`
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const r="/assets/logo-6Mk-ll80.jpg",c=()=>{const t=document.querySelector("header");t.innerHTML=`
     <div id="container-header">
       <div class="container-logo">
         <div class="logo-image">
@@ -13,10 +13,10 @@
         <li><i class="fa-solid fa-envelope icon-contact"></i><span class="text-menu"> Contact </span></li>
       </ul>
     </div>
-  `};c();const l=()=>{const t=document.querySelector("footer");t.innerHTML=`
+  `},l=()=>{const t=document.querySelector("footer");t.innerHTML=`
         <p>
         <img src="${r}" alt="logo" />
         copyright <i class="fa-regular fa-copyright"></i> 2024 -
         <span class="j">J</span>anvier<span class="e">E</span>ric
         </p>
-        `};l();
+        `},d=()=>{c(),l()};d();
